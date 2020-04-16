8d62dd7b57b0e6eb6447b8621ef232d5d1039ca1
John Kim <john@Johns-MacBook-Pro.local>
2020-04-16 15:45:16 -0700

    worked on databases sprint

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,12 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
 
+connection.connect();
+
+module.exports = connection;
